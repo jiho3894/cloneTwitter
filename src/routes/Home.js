ed0f5cc9899956ad7dac13d81b0deb38694b13e8
unknown <crsn1111@gmail.com>
2021-08-03 18:12:02 +0900

    write

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,4 +1,27 @@
-import React from 'react';
+import { dbService } from 'fbase';
+import React, { useState } from 'react';
 
-const Home = () => <span>Home</span>
+const Home = () => {
+  const [nweet, setNweet] = useState("");
+  const onSubmit = async (event) => {
+    event.preventDefault();
+    await dbService.collection("nweet").add({
+      nweet,
+      liveTime : Date.now()
+    });
+    setNweet("");
+  }
+  const onChange = (event) => {
+    const {target : {value}} = event;
+    setNweet(value);
+  }
+  return (
+    <div>
+      <form onSubmit={onSubmit}>
+        <input type="text" value={nweet} onChange={onChange} placeholder="당신의 상태를 적어주세요" maxLength={100}/>
+        <input type="submit" value="Nweet"/>
+      </form>
+    </div>
+  );
+}
 export default Home;
